159bde10f19139babc03b0d43b68359e5f386847
wangjuntao <6583805@qq.com>
2018-05-31 21:59:21 +0800

    deploy `date +m-H:S`

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,15 @@
-echo "www.toughcloud.net" > CNAME
+echo www.toughcloud.net > CNAME
 git add .
 git commit -am "deploy `date +%Y-%m-%d" "%H:%M:%S`"
 git push origin gh-pages
 
-echo "www.toughstruct.com" > CNAME
+echo www.toughstruct.com > CNAME
 git add .
 git commit -am "deploy `date +%Y-%m-%d" "%H:%M:%S`"
 git push tscom gh-pages
 
 
-echo "www.toughstruct.net" > CNAME
+echo www.toughstruct.net > CNAME
 git add .
 git commit -am "deploy `date +%Y-%m-%d" "%H:%M:%S`"
 git push tsnet gh-pages
